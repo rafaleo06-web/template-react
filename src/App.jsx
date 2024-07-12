ec5874138332953a6e5522c463119f810d7d2134
Rafael <rafa_061@hotmail.com>
2024-07-11 22:06:35 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { ArchiveBoxIcon } from "@heroicons/react/16/solid";
+
+export const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <ArchiveBoxIcon className="h-6 w-6"></ArchiveBoxIcon>
+    </h1>
+  );
+};
+
